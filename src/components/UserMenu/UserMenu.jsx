e521d2aba10ad81bf08d09c6b9e0b87641a2766f
Ruslan Negoda <iarjuna108@gmail.com>
2023-01-18 23:57:56 +0200

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { useAuth } from 'hook';
 import { UserBox, UserName } from './UserMenu.styled';
 import { logOut } from 'redux/auth/authOperations';
+import { contactsApi } from 'redux/sliceContacts';
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
@@ -11,7 +12,16 @@ export const UserMenu = () => {
   return (
     <UserBox>
       <UserName>Welcome, {user.name} </UserName>
-      <button type="button" onClick={() => dispatch(logOut())}>
+      <button
+        type="button"
+        onClick={() =>
+          dispatch(logOut())
+            .unwrap()
+            .then(() => {
+              contactsApi.util.resetApiState();
+            })
+        }
+      >
         Logout
       </button>
     </UserBox>
